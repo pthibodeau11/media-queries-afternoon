70e67e1c54fa8bf8b66eed6e9dc5336d1d04dbfb
Patrick Thibodeau <patrick.thibodeau@gmail.com>
2019-10-28 17:22:31 -0500

    520 push

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./Reset.css";
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      menuStatus: "menu-open",
+      menuRef: React.createRef()
+    };
+  }
+
+  handleClick = e => {
+    const node = this.state.menuRef.current;
+    if (node.className === "menu-open") {
+      node.className = "menu-close";
+    } else {
+      node.className = "menu-open";
+    }
+  };
+  render() {
+    return (
+      <>
+        <div>
+          <nav>
+            <h1>Start Bootstrap</h1>
+            <div className="ham">
+              <span>MENU</span>
+              <img
+                src="https://icon-library.net/images/menu-icon-white-png/menu-icon-white-png-27.jpg"
+                alt="ham_menu"
+                onClick={this.handleClick}
+              />
+            </div>
+            <ul>
+              <li>SERVICES</li>
+              <li>PORTFOLIO</li>
+              <li>ABOUT</li>
+              <li>TEAM</li>
+              <li>CONTACT</li>
+            </ul>
+          </nav>
+          <menu id="menu" ref={this.state.menuRef}>
+            <ul className="drop-down">
+              <li>SERVICES</li>
+              <li>PORTFOLIO</li>
+              <li>ABOUT</li>
+              <li>TEAM</li>
+              <li>CONTACT</li>
+            </ul>
+          </menu>
+          {/* <div id="drop-down" ref={this.state.menuRef}>
+              
+            </div> */}
+          <main>
+            <div className="container">
+              <h5>Welcome To Our Studio!</h5>
+              <h2>IT'S NICE TO MEET YOU</h2>
+              <button>TELL ME MORE</button>
+            </div>
+          </main>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
